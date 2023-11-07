a5b69fa06fee7e74d48dc6d2284393218dc0ec20
Vilguax <axel.pelassa@hotmail.fr>
2023-11-07 15:45:15 +0100

    Correction 19 Lister les officiers qui ont des associations avec des entités et des intermédiaires

--- ex19.sql
+++ ex19.sql
@@ -1,1 +1,11 @@
--- 
+-- Lister les officiers qui ont des associations avec des entités et des intermédiaires
+
+SELECT DISTINCT
+  o.id AS OfficerID,
+  o.name AS OfficerName
+FROM 
+  officer o
+JOIN 
+  assoc_officer_entity aoe ON o.id = aoe.officer
+JOIN 
+  assoc_officer_interm aoi ON o.id = aoi.officer;
