6e707300646dabeb7651c99568104ad4d48dac73
Vilguax <axel.pelassa@hotmail.fr>
2023-11-07 14:49:46 +0100

    Trouver tous les officiers et intermédiaires qui ont une association

--- ex8.sql
+++ ex8.sql
@@ -1,2 +1,11 @@
 -- Trouver tous les officiers et intermédiaires qui ont une association
 
+SELECT 
+  o.name AS OfficerName, 
+  i.name AS IntermediaryName
+FROM 
+  assoc_inter_offi aio
+JOIN 
+  officer o ON aio.officer = o.id
+JOIN 
+  intermediary i ON aio.inter = i.id;
